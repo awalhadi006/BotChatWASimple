557f1966c98327bb2974d28a8b89add347e58292
Awal Hadi Wijaya <awalhadi006@gmail.com>
2023-07-19 16:50:32 +0700

    Update otomatis kirim.py

--- otomatis kirim.py
+++ otomatis kirim.py
@@ -13,18 +13,29 @@ def import_data(filename):
     return data
 
 # Import data calon santri dari file CSV
-data = import_data('data_santri.csv')
+data = import_data('testing.csv')
 
 # Loop melalui data calon santri
 for index, santri in enumerate(data):
     nama = santri['Nama']
-    nomor_hp = santri['Nomor_HP']
+    nomor_hp_1 = santri['Nomor_HP1']
+    nomor_hp_2 = santri['Nomor_HP2']
+    nomor_hp_3 = santri['Nomor_HP3']
     berkas1 = santri['Berkas1']
     berkas2 = santri['Berkas2']
     berkas3 = santri['Berkas3']
     berkas4 = santri['Berkas4']
     berkas5 = santri['Berkas5']
     berkas6 = santri['Berkas6']
+
+    # Membuat daftar nomor HP
+    nomor_hp = []
+    if nomor_hp_1:
+        nomor_hp.append(nomor_hp_1)
+    if nomor_hp_2:
+        nomor_hp.append(nomor_hp_2)
+    if nomor_hp_3:
+        nomor_hp.append(nomor_hp_3)
 
     # Membuat daftar berkas yang dibutuhkan
     berkas_dibutuhkan = []
@@ -42,21 +53,25 @@ for index, santri in enumerate(data):
         berkas_dibutuhkan.append(berkas6)
 
     # Menentukan pesan berdasarkan data calon santri
-    pesan = f"_Assalamu'alaikum Wr. Wb_\n\nSelamat pagi Bapak/Ibu/Wali dari santri baru *{nama}*.\nSaya selaku admin dari Panitia Penerimaan Santri Baru (PSB) ingin menginformasikan bahwa *ada berkas yang harus dilengkapi untuk pendaftaran*, yaitu:\n"
+    pesan = f"_Assalamu'alaikum Wr. Wb_\n\nSelamat sore Bapak/Ibu/Wali dari santri baru *{nama}*.\nSaya selaku admin dari Panitia Penerimaan Santri Baru (PSB) ingin menginformasikan bahwa *ada beberapa berkas yang harus dilengkapi untuk pendaftaran*, yaitu:\n\n"
 
     for berkas in berkas_dibutuhkan:
         pesan += f"- *{berkas}*\n"
 
-    pesan += "\nHarap agar bisa dikumpulkan *hari ini*, paling lambat *hari Minggu, 24 Juli 2023* (berkasnya sementara bisa difotokan terlebih dahulu untuk penginputan datanya). Jika rumah Bapak/Ibu jauh dari pondok pesantren, berkas dapat dikirim via ekspedisi.\n\nTerima kasih.\n_Wassalamu'alaikum Wr. Wb_"
+    pesan += "\nHarap agar bisa dikumpulkan *hari ini*, paling lambat *hari Minggu, 24 Juli 2023* (berkasnya sementara bisa difotokan terlebih dahulu untuk penginputan datanya).\nJika rumah Bapak/Ibu jauh dari pondok pesantren, berkas dapat dikirim via ekspedisi.\n\nTerima kasih.\n_Wassalamu'alaikum Wr. Wb_"
 
-    try:
-        # Mengirim pesan WhatsApp pada jam sekarang
-        pywhatkit.sendwhatmsg_instantly(nomor_hp, pesan, 15, True, 10)
-        print(f"Message sent to {nama} | {nomor_hp}.")
-    except pywhatkit.exceptions.CountryCodeException:
-        print(f"Failed to send message to {nama} | {nomor_hp}. The number is not registered on WhatsApp.")
-    except Exception as e:
-        print(f"Failed to send message to {nama} | {nomor_hp}. Error: {str(e)}")
+    for nomor in nomor_hp:
+        try:
+            # Mengirim pesan WhatsApp pada jam sekarang ke setiap nomor HP
+            pywhatkit.sendwhatmsg_instantly(nomor, pesan, 15, True, 10)
+            print(f"Message sent to {nama} | {nomor}.")
+        except pywhatkit.exceptions.CountryCodeException:
+            print(f"Failed to send message to {nama} | {nomor}. The number is not registered on WhatsApp.")
+        except Exception as e:
+            print(f"Failed to send message to {nama} | {nomor}. Error: {str(e)}")
+
+        # Menunggu beberapa saat sebelum mengirim pesan ke nomor selanjutnya
+        time.sleep(10)
 
     # Jeda waktu sebelum pengiriman pesan berikutnya
     if index < len(data) - 1:
